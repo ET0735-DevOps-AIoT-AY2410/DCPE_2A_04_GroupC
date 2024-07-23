c3650388263296a0caceea1de63f3227eec200cd
RenzoGandicela <RENZOGANDICELA.23@ichat.sp.edu.sg>
2024-07-23 16:53:07 +0800

    Added accel_thread and workflow for force_detect.py

--- src/force_detect.py
+++ src/force_detect.py
@@ -1,3 +1,5 @@
+import time
+from threading import Thread
 from hal import hal_accelerometer as acc
 from hal import hal_buzzer as buzz
 
@@ -5,9 +7,18 @@ buzz.init()
 acc.init()
 acc.ADXL345().calibrate()
 
+def start(self):
+    # Start monitoring the accelerometer in a seperate thread
+    acc_thread = Thread(target=monitor_accelerometer, args=acc)
+    acc_thread.start()
+
+    acc_thread.join()
+    
 def monitor_accelerometer(acc):
     while True:
         x, y, z = acc.get_3_axis()
         total_g = (x**2 + y**2 + z**2)**0.5
         if total_g > 20:    # 20g threshold
             buzz.turn_on_with_timer(200)
+
+        time.sleep(0.1) # reduce the number of checks
