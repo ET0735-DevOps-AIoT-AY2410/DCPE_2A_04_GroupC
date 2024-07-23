867ea3f97c3624d7bf38a94cf1612fa33dcfb8d9
RenzoGandicela <RENZOGANDICELA.23@ichat.sp.edu.sg>
2024-07-23 15:21:21 +0800

    initial version of force_detect.py

--- src/force_detect.py
+++ src/force_detect.py
@@ -1,4 +1,13 @@
-from hal import hal_accelerometer as ACC
+from hal import hal_accelerometer as acc
+from hal import hal_buzzer as buzz
 
-def detect_force():
-    print()
+buzz.init()
+acc.init()
+acc.ADXL345().calibrate()
+
+def monitor_accelerometer(acc):
+    while True:
+        x, y, z = acc.get_3_axis()
+        total_g = (x**2 + y**2 + z**2)**0.5
+        if total_g > 20:    # 20g threshold
+            buzz.turn_on_with_timer(200)
