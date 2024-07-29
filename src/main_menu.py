82bd736d427257a642d1c030336ecb659e913e4e
RenzoGandicela <RENZOGANDICELA.23@ichat.sp.edu.sg>
2024-07-29 18:32:34 +0800

    edited main_menu.py

--- src/main_menu.py
+++ src/main_menu.py
@@ -5,31 +5,26 @@ import time
 import queue
 
 lcd = LCD.lcd()
+lcd.lcd_clear()
+
 testValue = 0
+shared_keypad_queue = queue.Queue()
+selected_option = None
 
-# Empty list to store sequence of keypad presses
-password = []
-selected_option = None  # Variable to store the selected option
-
-# Call back function invoked when any key on keypad is pressed
 def key_pressed(key):
-    global selected_option
-    password.append(key)
-    selected_option = key   # Update the selected option with the latest key press
-    print(f"Key pressed: {key}")  # Debugging statement
-    print(f"Updated selected_option: {selected_option}")  # Debugging statement
+    shared_keypad_queue.put()
 
-def display_menu(lcd):
+def display_menu():
     lcd.lcd_clear()
     lcd.lcd_display_string("1. Collect Drink", 1, 0)
     lcd.lcd_display_string("2. Purchase", 2, 0)
-    print("Menu displayed")  # Debugging statement
+    print("Menu displayed")
 
 def handle_user_selection():
     global selected_option, testValue
     while True:
         if selected_option is not None:
-            print(f"Handling selection: {selected_option}")  # Debugging statement
+            print(f"Handling selection: {selected_option}")
             if selected_option == '1':
                 lcd.lcd_clear()
                 lcd.lcd_display_string("Face QR Code", 1, 0)
@@ -40,20 +35,19 @@ def handle_user_selection():
                 lcd.lcd_display_string("1. Milo", 1, 0)
                 lcd.lcd_display_string("2. 100 Plus", 2, 0)
                 testValue = 2
-            selected_option = None  # Reset selected option after handling
-        time.sleep(0.1)  # Small delay to prevent busy-waiting
+            selected_option = None
+        time.sleep(0.1)
+
+def monitor_keypad():
+    keypad.init(key_pressed)
+    keypad.get_key()
 
 def main_menu_flow():
-    try:
-        # Display main menu
-        display_menu(lcd)
-
-        # Initialise the HAL keypad driver
-        keypad.init(key_pressed)
-
-        # Start a thread to handle keypad input
-        keypad_thread = Thread(target=keypad.get_key)
-        keypad_thread.start()
+    try:    
+        display_menu()
+        
+        # Run monitor_keypad in the main thread
+        monitor_keypad()
 
         # Start a thread to handle user selection
         selection_thread = Thread(target=handle_user_selection)
@@ -61,7 +55,6 @@ def main_menu_flow():
 
         # Main thread can continue to do other things, or just wait for user selection to be handled
         selection_thread.join()
-        keypad_thread.join()
 
     except Exception as e:
         print(f"An error occurred: {e}")
