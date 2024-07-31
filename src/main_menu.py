3682be3d9b34d139eeb71336becc58bf53eb3d65
RenzoGandicela <RENZOGANDICELA.23@ichat.sp.edu.sg>
2024-07-31 18:57:16 +0800

    changed main_menu.py

--- src/main_menu.py
+++ src/main_menu.py
@@ -18,29 +18,34 @@ def display_menu():
     lcd.lcd_clear()
     lcd.lcd_display_string("1. Collect Drink", 1, 0)
     lcd.lcd_display_string("2. Purchase", 2, 0)
-    print("Menu displayed")
 
 def handle_user_selection():
     global selected_option, testValue
-    while True:
-        if selected_option is not None:
-            print(f"Handling selection: {selected_option}")
-            if selected_option == '1':
-                lcd.lcd_clear()
-                lcd.lcd_display_string("Face QR Code", 1, 0)
-                lcd.lcd_display_string("towards camera", 2, 0)
-                testValue = 3
-            elif selected_option == '2':
-                lcd.lcd_clear()
-                lcd.lcd_display_string("1. Milo", 1, 0)
-                lcd.lcd_display_string("2. 100 Plus", 2, 0)
-                testValue = 2
-            selected_option = None
-        time.sleep(0.1)
+    if selected_option is not None:
+        print(f"Handling selection: {selected_option}")
+        if selected_option == '1':
+            lcd.lcd_clear()
+            lcd.lcd_display_string("Face QR Code", 1, 0)
+            lcd.lcd_display_string("towards camera", 2, 0)
+            testValue = 3
+        elif selected_option == '2':
+            lcd.lcd_clear()
+            lcd.lcd_display_string("1. Milo", 1, 0)
+            lcd.lcd_display_string("2. 100 Plus", 2, 0)
+            testValue = 2
+        selected_option = None
+    time.sleep(0.1)
 
 def main_menu_flow():
-    display_menu()
-        
+
+    # Initialisation of HAL modules
+    lcd = LCD.lcd()
+    lcd.lcd_clear()
+
+    keypad.init(key_pressed)
+    keypad_thread = Thread(target=keypad.get_key)
+    keypad_thread.start()
+
     # Run monitor_keypad in the main thread
     keypad.init(key_pressed)
     keypad_thread = Thread(target=keypad.get_key)
@@ -49,10 +54,13 @@ def main_menu_flow():
     # Start a thread to handle user selection
     selection_thread = Thread(target=handle_user_selection)
     selection_thread.start()
+    
+    while True:
+        print("key value ", selected_option)
 
-    # Main thread can continue to do other things, or just wait for user selection to be handled
-    selection_thread.join()
-    keypad_thread.join()
+        display_menu()
+        handle_user_selection()
+
 
 if __name__ == "__main__":
     main_menu_flow()
