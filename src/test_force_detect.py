2fbcb536bf8821a31aec48863c8d87d7bacf6a53
RenzoGandicela <RENZOGANDICELA.23@ichat.sp.edu.sg>
2024-08-03 00:37:28 +0800

    deleted rubbish in test_force

--- src/test_force_detect.py
+++ src/test_force_detect.py
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import force_detect as test_acc
-
-def test_accelerometer():
-   test_acc.start()
-   g_force = 30
-   test_acc.monitor_accelerometer(g_force)
-   assert test_acc.monitor
-=======
-def test_accelerometer():
-    import force_detect
-    g_force = 30
-    force_detect.monitor_accelerometer(g_force)
-    
-    assert force_detect.test_value == 3 
->>>>>>> renzo
-=======
 import pytest
 from unittest.mock import MagicMock, patch
 
@@ -59,4 +41,3 @@ def test_monitor_accelerometer(mock_print, mock_beep, mock_acc_init):
 
 if __name__ == "__main__":
     pytest.main()
->>>>>>> renzo
