a8a1e538dabe09e43652430627977d3854b1eec4
ERZXErvin <ERZX.23@ichat.sp.edu.sg>
2024-08-10 00:22:25 +0800

    qr code generator update

--- src/qr_code_generator.py
+++ src/qr_code_generator.py
@@ -1,18 +1,25 @@
 import qrcode
 import random
 import qr_code_data
+import os
 
 def qr_generate():
+    file_name = "qr-img.jpg"
+    if os.path.exists(file_name):
+        os.remove(file_name)
     qr_code_data.qr_data = random.randint(0, 100)
     print(f"Generated QR Data: {qr_code_data.qr_data}")
     qr_img = qrcode.make(qr_code_data.qr_data)
-    qr_img.save("qr-img.jpg")
+    qr_img.save(file_name)
 
 def qr_generatepay():
+    file_name = "qr-pay.jpg"
+    if os.path.exists(file_name):
+        os.remove(file_name)
     qr_code_data.qr_data = random.randint(0, 100)
     print(f"Generated QR Data: {qr_code_data.qr_data}")
     qr_img = qrcode.make(qr_code_data.qr_data)
-    qr_img.save("qr-pay.jpg")
+    qr_img.save(file_name)
 
 if __name__ == '__main__':
     qr_generate()
